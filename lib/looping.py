94182063d752354ea2b33907701cc355bbf1b011
Christopher <chris.brower.96@gmail.com>
2023-03-14 10:05:45 -0600

    commit

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,26 @@
 
 def happy_new_year():
     # code goes here!
+    for i in range(10):
+        print(f"{10-i}")
+    print("Happy New Year!")
     pass
-
+happy_new_year()
 def square_integers(int_list):
     # code goes here!
+    squared_list = [this_int*this_int for this_int in int_list]
+    return squared_list
     pass
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
+
